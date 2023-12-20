89a405cdffedb22c2c15b229a289b4b4a2bc072d
Javierdds <deduenassantanojavier@gmail.com>
2023-12-20 11:58:59 +0100

    FIX: Tests for not logged users

--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -1,7 +1,9 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 using NUnit.Framework;
 using TripServiceKata.Exception;
 using TripServiceKata.Trip;
+using TripServiceKata.User;
 
 namespace TripServiceKata.Tests
 {
@@ -15,29 +17,58 @@ namespace TripServiceKata.Tests
             _tripService = new TripService();
         }
 
-        [Test]
-        public void GivenNullUser_ByDefinition_AnExceptionIsThrown()
+        [TearDown]
+        public void TearDown()
         {
-            Assert.Throws<UserNotLoggedInException>(
+            _tripService = null;
+        }
+
+        [Test]
+        public void GivenNullFriendUser_ByDefinition_AnExceptionIsThrown()
+        {
+            _tripService = new TripServiceForLoggedUser();
+            
+            Assert.Throws<System.NullReferenceException>(
                 () => _tripService.GetTripsByUser(null));
         }
         
         [Test]
         public void GivenGuestUser_ByDefinition_AnExceptionIsThrown()
         {
-            User.User guestUser = new User.User();
+            var user = new User.User();
+            _tripService = new TripServiceForGuestUser();
             
             Assert.Throws<UserNotLoggedInException>(
-                () => _tripService.GetTripsByUser(guestUser));
+                () => _tripService.GetTripsByUser(user));
         }
 
         [Test]
         public void GivenLoggedUser_WithNoFriends_GetsEmptyTripsAsResult()
         {
+            var user = new User.User();
+            var expectedValue = new List<Trip.Trip>();
+            _tripService = new TripServiceForLoggedUser();
+
+            var tripsByUser = _tripService.GetTripsByUser(user);
             
+            Assert.AreEqual(tripsByUser, expectedValue);
+        }
+
+        private class TripServiceForLoggedUser : TripService
+        {
+            protected override User.User GetLoggedUser()
+            {
+                return new User.User();
+            }
         }
         
-        
+        private class TripServiceForGuestUser : TripService
+        {
+            protected override User.User GetLoggedUser()
+            {
+                return null;
+            }
+        }
         
         // private void AssertAmount(int expected, string failMessage)
         // {
